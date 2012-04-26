88301577450052e33fa67d256d7a59c3d01763da
virgildisgr4ce <daleth@epiphanus.net>
2012-04-26 18:35:34 -0400

    Rearranged & Updated

---
+++ init-project.sh
@@ -0,0 +1,35 @@
+# Project creation script
+# (for Tornado on Heroku)
+#
+# by Mike Dory | dory.me
+# 11.12.11
+# modified by Tedb0t | tedbot.com
+
+# --------------------------
+
+# git!
+git init
+
+# --------------------------
+
+# set up the pip requirements
+touch requirements.txt
+echo "Tornado==2.1.1" >> requirements.txt
+
+# set up the Procfile
+touch Procfile
+echo "web: python main.py" >> Procfile
+
+# --------------------------
+
+echo "Committing to Git"
+
+git add .
+git commit -m "Initial Commit"
+
+echo "Creating Heroku app & pushing"
+
+heroku create --stack cedar
+git push heroku master
+
+echo "All Done!"
